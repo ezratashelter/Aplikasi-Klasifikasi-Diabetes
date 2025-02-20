b777ad26ceeeb5febaa636afb7f471ddfb57b2cb
ezratashelter <96658470+ezratashelter@users.noreply.github.com>
2025-02-21 02:03:16 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+# Judul aplikasi
+st.title("Prediksi Diabetes dengan SVM")
+
+# Deskripsi aplikasi
+st.write("Masukkan data kesehatan Anda untuk mengetahui risiko diabetes.")
+
+
+# Load model
+def load_model():
+    with open("svm_model.pkl", "rb") as f:
+        model, scaler = pickle.load(f)
+    return model, scaler
+
+model, scaler = load_model()
+
+# Input data dari user
+col1, col2 = st.columns(2)
+
+with col1 :
+    pregnancies = st.number_input("Jumlah Kehamilan", min_value=0, max_value=20)
+with col2 :
+    glucose = st.number_input("Glukosa", min_value=0, max_value=200,)
+with col1 :
+    blood_pressure = st.number_input("Tekanan Darah", min_value=0, max_value=150)
+with col2 :
+    skin_thickness = st.number_input("Ketebalan Kulit", min_value=0, max_value=100)
+with col1 :
+    insulin = st.number_input("Insulin", min_value=0, max_value=900)
+with col2 :
+    bmi = st.number_input("BMI", min_value=0.0, max_value=70.0)
+with col1 :
+    dpf = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.5)
+with col2 :
+    age = st.number_input("Usia", min_value=0, max_value=120)
+
+# Prediksi
+if st.button("Prediksi"):
+    input_data = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
+    input_scaled = scaler.transform(input_data)
+    prediction = model.predict(input_scaled)
+    probability = model.predict_proba(input_scaled)[0][1]
+
+    if prediction[0] == 1:
+        st.error(f"Prediksi: **Pasien Terkena Diabetes**")
+    else:
+        st.success(f"Prediksi: **Pasien Tidak Diabetes**")
